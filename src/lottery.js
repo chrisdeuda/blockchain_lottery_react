a9cd419df5e7243e89cf9575c9022fafaaf878ff
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-18 00:32:08 +0800

    Add package dotenv-webpack

---
+++ src/lottery.js
@@ -0,0 +1,7 @@
+import web3 from './web3';
+const address ="0xB1497E00744f8F5c743E80D9fddfE6D46699e987";
+const abi = [{"constant":true,"inputs":[],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"manager","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pickWinner","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getPlayers","outputs":[{"name":"","type":"address[]"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"enter","outputs":[],"payable":true,"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"players","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]
+
+
+// Creating the instance of the contract to be  use in the React JS
+export default new web3.eth.Contract(abi,address);
