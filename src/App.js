980944e1eaa6e4b4e634ba54bf2a4f4fa9c93113
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-18 22:01:22 +0800

    Add pick winner

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
     const  manager = await lottery.methods.manager().call();
     const  players = await lottery.methods.getPlayers().call();
     const balance = await web3.eth.getBalance(lottery.options.address);
-    this.setState({manager: manager, balance});
+    this.setState({manager: manager, balance, players});
   }
 
   onSubmit = async (event) => {
@@ -33,6 +33,19 @@ class App extends Component {
     });
 
     this.setState({message: 'You have been entered!'});
+
+  }
+
+  onClick = async (event) =>{
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0]
+    });
+
+    this.setState({message: 'A winner has been picked'});
 
   }
 
@@ -57,6 +70,10 @@ class App extends Component {
           <button>Enter</button>
 
         </form>
+        <hr/>
+        <h4> Ready to pick a winner ? </h4>
+        <button onClick={this.onClick}>Pick a winner</button>
+
         <hr/>
         <h1>{ this.state.message}</h1>
      </div>
