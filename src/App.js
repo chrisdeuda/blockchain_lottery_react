896bbc504300cf11779d6b15e031b402fd7ae0f7
Christopher Deuda <christopherdeuda@gmail.com>
2018-10-18 20:00:01 +0800

    Add display of the contract values

--- src/App.js
+++ src/App.js
@@ -1,27 +1,64 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Web3 from './web3';
+import web3 from './web3';
 import lottery from './lottery';
 
 class App extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = { manager: ''};
+  state = {
+    manager : '',
+    players: [],
+    balance: '',
+    value: '',
   }
 
   async componentDidMount(){
     const  manager = await lottery.methods.manager().call();
-    this.setState({manager: manager});
+    const  players = await lottery.methods.getPlayers().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
+    this.setState({manager: manager, balance});
+  }
+
+  onSubmit = async (event) => {
+    event.preventDefault();
+
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    //Sending transactions to the network
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei(this.state.value, 'ether')
+    });
+
+    this.setState({message: 'You have been entered!'});
+
   }
 
   render() {
     return (
      <div>
         <h2>Lottery Contract</h2>
-        <p> This contract is managed by {this.state.manager}</p>
+        <p> This contract is managed by {this.state.manager}.
+          There are currently {this.state.players.length} people entered,
+          competing to win { web3.utils.fromWei(this.state.balance)} ether !
+        </p>
+        <hr />
+        <form onSubmit={this.onSubmit}>
+          <h4> Want to try your luck ?</h4>
+          <div>
+            <label> Amount of ether to enter </label>
+            <input 
+              value={this.state.value}
+              onChange={event => this.setState({ value: event.target.value})}
+            />
+          </div>
+          <button>Enter</button>
 
+        </form>
+        <hr/>
+        <h1>{ this.state.message}</h1>
      </div>
     );
   }
